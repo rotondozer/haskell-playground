c50ed1eb6642f66d981fcc1344071b21b689eb36
Nick Rotondo <nickrotondo724@gmail.com>
2020-06-06 16:30:16 -0400

    change lib to only export a single function

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,8 @@
 module Main where
 
-import           Lib                            ( getAmountIndex
-                                                , toRowsAndColumns
-                                                , total
-                                                , CashFlow(..)
-                                                )
+import           Lib                            ( monthlyExpenseReport )
 import           System.IO
 
 main = do
     contents <- readFile "../../Downloads/EXPORT.CSV"
-    let rowsAndColumns = toRowsAndColumns contents
-    case (getAmountIndex rowsAndColumns) of
-        Nothing -> putStrLn "whoops!"
-        Just index ->
-            let (debits, credits) =
-                        ( (total Debit index rowsAndColumns)
-                        , (total Credit index rowsAndColumns)
-                        )
-            in  print
-                    $  "DEBITS: "
-                    ++ (show debits)
-                    ++ " / CREDITS: "
-                    ++ (show credits)
-                    ++ " / NET: "
-                    ++ (show (credits + debits))
+    print $ monthlyExpenseReport contents
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,11 @@
 module Lib
-    ( getAmountIndex
-    , toRowsAndColumns
-    , total
-    , CashFlow(..)
+    ( monthlyExpenseReport
     )
 where
 
 import           Data.List
 import           Data.List.Split
+import           Data.Maybe                     ( fromMaybe )
 import           Text.Read
 
 type Row = [String]
@@ -23,9 +21,7 @@ getAmountIndex :: RowsAndColumns -> Maybe Int
 getAmountIndex rowsAndColumns = elemIndex "\"Amount\"" (head rowsAndColumns)
 
 getAmountFromRow :: Int -> Row -> Float
-getAmountFromRow amountIndex row = case amount of
-    Nothing -> 0
-    Just a  -> a
+getAmountFromRow amountIndex row = fromMaybe 0 amount
     where amount = readMaybe (row !! amountIndex) :: Maybe Float
 
 total :: CashFlow -> Int -> RowsAndColumns -> Float
@@ -42,3 +38,19 @@ isOfCashFlowType amount cashFlow = case cashFlow of
     Debit  -> (amount < 0.0)
     Credit -> (amount > 0.0)
 
+monthlyExpenseReport :: String -> String
+monthlyExpenseReport csvFileContents =
+    let rowsAndColumns = toRowsAndColumns csvFileContents
+    in  case (getAmountIndex rowsAndColumns) of
+            Nothing -> "whoops!"
+            Just index ->
+                let (debits, credits) =
+                            ( (total Debit index rowsAndColumns)
+                            , (total Credit index rowsAndColumns)
+                            )
+                in  "DEBITS: "
+                        ++ (show debits)
+                        ++ " / CREDITS: "
+                        ++ (show credits)
+                        ++ " / NET: "
+                        ++ (show $ credits + debits)
