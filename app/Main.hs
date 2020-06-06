e66c367887e4f45a19a3d8d8a26d46d206254372
Nick Rotondo <nickrotondo724@gmail.com>
2020-06-06 12:33:31 -0400

    Move functions to Lib

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,6 @@ module Main where
 
 import           Lib
 import           System.IO
-import           Data.List
-import           Data.List.Split
-import           Text.Read
 
 main = do
     contents <- readFile "../../Downloads/EXPORT.CSV"
@@ -23,33 +20,3 @@ main = do
                     ++ (show credits)
                     ++ " / NET: "
                     ++ (show (credits + debits))
-
-
-toRowsAndColumns :: String -> [[String]]
-toRowsAndColumns contents = map (splitOneOf ",\\") tableRows
-    where tableRows = lines contents
-
-getAmountIndex :: [[String]] -> Maybe Int
-getAmountIndex rowsAndColumns = elemIndex "\"Amount\"" (head rowsAndColumns)
-
-getAmountFromRow :: Int -> [String] -> Float
-getAmountFromRow amountIndex row = case amount of
-    Nothing -> 0
-    Just a  -> a
-    where amount = readMaybe (row !! amountIndex) :: Maybe Float
-
-totalDebits :: Int -> [[String]] -> Float
-totalDebits amountIndex = foldl
-    (\acc row ->
-        if (amountFromRow row) < 0 then acc + (amountFromRow row) else acc
-    )
-    0
-    where amountFromRow = getAmountFromRow amountIndex
-
-totalCredits :: Int -> [[String]] -> Float
-totalCredits amountIndex = foldl
-    (\acc row ->
-        if (amountFromRow row) > 0 then acc + (amountFromRow row) else acc
-    )
-    0
-    where amountFromRow = getAmountFromRow amountIndex
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,41 @@
 module Lib
-    ( someFunc
+    ( getAmountFromRow
+    , getAmountIndex
+    , toRowsAndColumns
+    , totalCredits
+    , totalDebits
     )
 where
 
-someFunc :: Int
-someFunc = sum (takeWhile (< 10000) (filter odd (map (^ 2) [1 ..])))
+import           Data.List
+import           Data.List.Split
+import           Text.Read
+
+toRowsAndColumns :: String -> [[String]]
+toRowsAndColumns contents = map (splitOneOf ",\\") tableRows
+    where tableRows = lines contents
+
+getAmountIndex :: [[String]] -> Maybe Int
+getAmountIndex rowsAndColumns = elemIndex "\"Amount\"" (head rowsAndColumns)
+
+getAmountFromRow :: Int -> [String] -> Float
+getAmountFromRow amountIndex row = case amount of
+    Nothing -> 0
+    Just a  -> a
+    where amount = readMaybe (row !! amountIndex) :: Maybe Float
+
+totalDebits :: Int -> [[String]] -> Float
+totalDebits amountIndex = foldl
+    (\acc row ->
+        if (amountFromRow row) < 0 then acc + (amountFromRow row) else acc
+    )
+    0
+    where amountFromRow = getAmountFromRow amountIndex
+
+totalCredits :: Int -> [[String]] -> Float
+totalCredits amountIndex = foldl
+    (\acc row ->
+        if (amountFromRow row) > 0 then acc + (amountFromRow row) else acc
+    )
+    0
+    where amountFromRow = getAmountFromRow amountIndex
