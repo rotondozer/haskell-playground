5a4b11b5dcb62b3230480e771a5efb9d30d148f5
Nick Rotondo <nickrotondo724@gmail.com>
2020-06-06 19:15:09 -0400

    add toCashFlow, change total to return Tuple

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,7 +11,7 @@ import           Text.Read
 type Row = [String]
 type RowsAndColumns = [Row]
 
-data CashFlow = Credit | Debit
+data CashFlow = Credit Float | Debit Float
 
 toRowsAndColumns :: String -> RowsAndColumns
 toRowsAndColumns contents = map (splitOneOf ",\\") tableRows
@@ -24,33 +24,30 @@ getAmountFromRow :: Int -> Row -> Float
 getAmountFromRow amountIndex row = fromMaybe 0 amount
     where amount = readMaybe (row !! amountIndex) :: Maybe Float
 
-total :: CashFlow -> Int -> RowsAndColumns -> Float
-total cashFlow columnIndex = foldl
-    (\acc row ->
-        let amount = amountFromRow row
-        in  if (amount `isOfCashFlowType` cashFlow) then acc + amount else acc
+
+total :: Int -> RowsAndColumns -> (Float, Float) -- TODO: type (NegativeFloat, ZeroOrGreaterFloat)
+total columnIndex = foldl
+    (\(debits, credits) row -> case (toCashFlow (amountFromRow row)) of
+        Debit  d -> (debits + d, credits)
+        Credit c -> (debits, credits + c)
     )
-    0
+    (0, 0)
     where amountFromRow = getAmountFromRow columnIndex
 
-isOfCashFlowType :: Float -> CashFlow -> Bool
-isOfCashFlowType amount cashFlow = case cashFlow of
-    Debit  -> (amount < 0.0)
-    Credit -> (amount > 0.0)
+toCashFlow :: Float -> CashFlow
+toCashFlow amount | amount < 0.0 = Debit amount
+                  | otherwise    = Credit amount
+
 
 monthlyExpenseReport :: String -> String
-monthlyExpenseReport csvFileContents =
-    let rowsAndColumns = toRowsAndColumns csvFileContents
-    in  case (getAmountIndex rowsAndColumns) of
-            Nothing -> "whoops!"
-            Just index ->
-                let (debits, credits) =
-                            ( (total Debit index rowsAndColumns)
-                            , (total Credit index rowsAndColumns)
-                            )
-                in  "DEBITS: "
-                        ++ (show debits)
-                        ++ " / CREDITS: "
-                        ++ (show credits)
-                        ++ " / NET: "
-                        ++ (show $ credits + debits)
+monthlyExpenseReport csvFileContents = case (getAmountIndex rowsAndColumns) of
+    Nothing -> "ERROR: Could not find \"Amount\" column in CSV"
+    Just index ->
+        let (debits, credits) = total index rowsAndColumns
+        in  "DEBITS: "
+                ++ (show debits)
+                ++ " / CREDITS: "
+                ++ (show credits)
+                ++ " / NET: "
+                ++ (show $ credits + debits)
+    where rowsAndColumns = toRowsAndColumns csvFileContents
